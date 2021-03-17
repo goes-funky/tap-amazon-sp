7f8fff05c81ec2712fae6f682b11336586ed6ab8
mohamedfawzy96 <mohamedfawzydes@gmail.com>
2021-03-17 11:45:30 +0100

    fixing issue with datetime report

--- tap_amazon_sp/streams/transaction_report.py
+++ tap_amazon_sp/streams/transaction_report.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta
+
 from tap_amazon_sp.streams.report import Report
+from email.utils import parsedate_tz, mktime_tz
+from singer import utils
 
 
 class TransactionReport(Report):
@@ -6,6 +10,14 @@ class TransactionReport(Report):
     name = "transactions_report"
     api_access_key = None
     replication_method = "FULL_IMPORT"
+    key_properties = []
 
     def clean_data(self, data):
         return data[7:]
+
+    def transform_fields(self, obj):
+        if "date_time" in obj:
+            timestamp = mktime_tz(parsedate_tz(obj["date_time"]))
+            utc_dt = datetime(1970, 1, 1) + timedelta(seconds=timestamp)
+            obj["date_time"] = utc_dt.ctime()
+        return obj
